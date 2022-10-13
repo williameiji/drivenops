07ae755f2045462bdee72d84d0faa68e2f8c5277
williameiji <william.fujimori@gmail.com>
2022-10-13 10:45:57 -0300

    test pr workflow

--- back-end/tests/integration/app.test.ts
+++ back-end/tests/integration/app.test.ts
@@ -1,26 +1,28 @@
-import supertest from 'supertest';
-import app from '../../src/app.js';
-import prisma from '../../src/database.js';
+import supertest from "supertest";
+import app from "../../src/app.js";
+import prisma from "../../src/database.js";
 
 const agent = supertest(app);
 
-describe('integration test', () => {
-  beforeEach(async () => {
-    await prisma.student.deleteMany();
-  });
+describe("integration test", () => {
+	beforeEach(async () => {
+		await prisma.student.deleteMany();
+	});
 
-  it('should save a student', async () => {
-    const students = { students: [{ name: 'juvelina' }] };
-    const { status } = await agent.post('/students').send(students);
-    expect(status).toBe(201);
+	it("should save a student", async () => {
+		const students = { students: [{ name: "juvelina" }] };
+		const { status } = await agent.post("/students").send(students);
+		expect(status).toBe(201);
 
-    // side effect
-    const savedStudent = await prisma.student.findFirst({
-      where: {
-        name: 'juvelina'
-      }
-    });
+		// side effect
+		const savedStudent = await prisma.student.findFirst({
+			where: {
+				name: "juvelina",
+			},
+		});
 
-    expect(savedStudent).not.toBeNull();
-  });
+		expect(savedStudent).not.toBeNull();
+	});
 });
+
+//test pr workflow
